56850e1ac25b5fcf8be9e92d8de787034c9b89a0
bendzsigolo <106239113+bendzsigolo@users.noreply.github.com>
2023-05-23 13:06:17 +0200

    Add files via upload

---
+++ fappjuniusifeladat/fappjuniusifeladat/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Data.SqlClient;
+namespace WpfLogin
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        static String connectionString = @"Data Source=192.168.0.192;Initial Catalog=DBCustomer;User ID=sa;Password=1234;";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click_1(object sender, RoutedEventArgs e)
+        {
+            String message = "Invalid Credentials";
+            try
+            {
+                con = new SqlConnection(connectionString);
+                con.Open();
+                cmd = new SqlCommand("Select * from tblCustomerInfo where CustomerEmail=@CustomerEmail", con);
+                cmd.Parameters.AddWithValue("@CustomerEmail", txtUserId.Text.ToString());
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    if (reader["Password"].ToString().Equals(txtPassword.Password.ToString(), StringComparison.InvariantCulture))
+                    {
+                        message = "1";
+                        UserInfo.CustomerEmail = txtUserId.Text.ToString();
+                        UserInfo.CustomerName = reader["CustomerName"].ToString();
+                    }
+                }
+
+                reader.Close();
+                reader.Dispose();
+                cmd.Dispose();
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message.ToString();
+            }
+            if (message == "1")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show(message, "Info");
+        }
+
+        private void btnClose_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
